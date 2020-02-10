c0c363cb7fbec28703de600602765a87ac9f4b66
akshay chaturvedi <a.chaturvedi332@gmail.com>
2020-02-10 12:13:56 +0530

    redis task using python

--- RedisPractice/redisPract.py
+++ RedisPractice/redisPract.py
@@ -1,3 +1,19 @@
 import redis
+import time
 
-conn = redis.Redis()
+conn = redis.Redis(host='127.0.0.1', port='6379')
+
+conn.set('language', 'python')
+print(conn.get("language"))
+
+conn.set('language', 'python', px=10000)
+print(conn.get('language'))
+print(conn.ttl('language'))
+time.sleep(3)
+print(conn.ttl('language'))
+
+conn.set('language', 'python', px=10000)
+print(conn.expire('language', 10))
+print(conn.ttl('language'))
+time.sleep(3)
+print(conn.ttl('language'))
