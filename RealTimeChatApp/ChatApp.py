6fd3350e57d3a18da84f9db56104a1affbcb3e55
akshay chaturvedi <a.chaturvedi332@gmail.com>
2020-02-12 12:37:22 +0530

    updated
    retrieving data of sent and received messages from redis

--- RealTimeChatApp/ChatApp.py
+++ RealTimeChatApp/ChatApp.py
@@ -1,6 +1,8 @@
 import ast
+import datetime
 import socket
 import time
+import sys
 import traceback
 from pprint import pprint
 import threading
@@ -9,31 +11,52 @@ import redis
 # tlock = threading.Lock()
 shutdown = False
 
+sent_data = {}
+sent_list_name = 'sent_data'
+recv_messages = {}
+recv_list_name = 'recv_data'
+
 
 def receiving(name, sock):
     while not shutdown:
         try:
             while True:
-                data, addr = sock.recvfrom(1024)
-                print('\n'+name+'> '+str(data.decode()))
+                if sock:
+                    data, addr = sock.recvfrom(1024)
+                    print('\n'+name+'> '+str(data.decode()))
+                    time_stamp = time.time()
+                    recv_time = datetime.datetime.fromtimestamp(time_stamp).strftime('%Y-%m-%d %H:%M:%S')
+                    recv_info = [hostname, data.decode(), ip_address, socket.gethostbyname(hostname), recv_time]
+                    recv_messages[name] = recv_info
+                    redis_client.lpush(recv_list_name, str(recv_messages))
+                    # print(recv_messages)
+                else:
+                    break
+
         except Exception as exception:
             print('receive failed!!!!!!'+str(exception))
-            pass
 
 
-def sending(sock):
+def sending(sender, sock):
     while not shutdown:
         try:
-            message = input(hostname + "> ")
+            message = input(sender + "> ")
             while True:
                 while message != 'q':
                     if message != '':
                         sock.sendto(bytes(message, 'utf-8'), (ip_address, port))
-                    message = input(hostname + '> ')
-                    time.sleep(0.2)
+                        time_stamp = time.time()
+                        sent_time = datetime.datetime.fromtimestamp(time_stamp).strftime('%Y-%m-%d %H:%M:%S')
+                        sent_info = [con_user, message, socket.gethostbyname(hostname), ip_address, sent_time]
+                        sent_data[hostname] = sent_info
+                        redis_client.lpush(sent_list_name, str(sent_data))
+                    message = input(sender + '> ')
+                    # time.sleep(0.2)
+                sock.sendto(bytes('user disconnected', 'utf-8'), (ip_address, port))
+                sock.close()
+                break
         except Exception as exception:
             print("send failed!!!!!!"+str(exception))
-            pass
 
 
 # register a user
@@ -41,49 +64,69 @@ hostname = socket.gethostname()
 port = 8888
 
 try:
-    hash_set_name = 'user_data'
-    redis_client = redis.Redis(charset='utf-8', decode_responses=True)
-    choice = input("you want to register?(yes/no): ")
-    if choice.lower() == 'y':
-        user_name = input("Enter your name: ")
-        ip_address = input("Enter IP Address: ")
-        user_data = [user_name, ip_address]
-        redis_client.lpush(hash_set_name, str(user_data))
-        print('registered successfully!!!!!')
-        # ast.literal_eval(user_data)
-        # print(user_data)
-    user_reg_data = {}
+    ans = 'y'
+    while ans.lower() == 'y':
+        user_data_list = 'user_data'
+        redis_client = redis.Redis(charset='utf-8', decode_responses=True)
+        print('\n--------select actions--------\n1. register user\n2. view sender data\n3. view receiver data\n4. chat')
+        choice = int(input("choose option: "))
+        if choice == 1:
+            user_name = input("Enter your name: ")
+            ip_address = input("Enter IP Address: ")
+            user_data = [user_name, ip_address]
+            redis_client.lpush(user_data_list, str(user_data))
+            print('registered successfully!!!!!')
+            # ast.literal_eval(user_data)
+            # print(user_data)
+        elif choice == 2:
+            search_name = input('Enter a name to search: ')
+            for index in range(0, redis_client.llen(sent_list_name)):
+                sent_dict = ast.literal_eval(redis_client.lindex(sent_list_name, index))
+                if search_name in sent_dict.keys():
+                    print(sent_dict.values())
+        elif choice == 3:
+            name_search = input('Enter a name to search: ')
+            for ind in range(0, redis_client.llen(recv_list_name)):
+                recv_dict = ast.literal_eval(redis_client.lindex(recv_list_name, ind))
+                if name_search in recv_dict.keys():
+                    print(recv_dict.values())
+        elif choice == 4:
+            user_reg_data = {}
 
-    for i in range(0, redis_client.llen(hash_set_name)):
-        user_reg_data[i] = redis_client.lindex(hash_set_name, i)
+            for i in range(0, redis_client.llen(user_data_list)):
+                user_reg_data[i] = redis_client.lindex(user_data_list, i)
 
-    con_user = input("enter name: ")
-    ip_address = []
-    for key, value in user_reg_data.items():
-        if str(value).find(con_user) != -1:
-            ip_address = str(value).split(',')
-            ip_address = [x.strip() for x in ip_address]
-            ip_address = [x.replace('[', '') for x in ip_address]
-            ip_address = [x.replace(']', '') for x in ip_address]
-            ip_address = [x.replace("'", '') for x in ip_address]
-            ip_address = ip_address[1]
-            break
+            con_user = input("enter name: ")
+            ip_address = []
+            for key, value in user_reg_data.items():
+                if str(value).find(con_user) != -1:
+                    ip_address = str(value).split(',')
+                    ip_address = [x.strip() for x in ip_address]
+                    ip_address = [x.replace('[', '') for x in ip_address]
+                    ip_address = [x.replace(']', '') for x in ip_address]
+                    ip_address = [x.replace("'", '') for x in ip_address]
+                    ip_address = ip_address[1]
+                    break
+                else:
+                    print('no such user found, try to register first')
+                    sys.exit(0)
 
-    # connect to a user
-    soc_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    soc_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    soc_conn.bind((hostname, port))
-    # soc_conn.setblocking(0)
+            # connect to a user
+            soc_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            soc_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            soc_conn.bind((hostname, port))
+            # soc_conn.setblocking(0)
 
-    rec_thread = threading.Thread(target=receiving, args=("RecThread", soc_conn))
-    send_thread = threading.Thread(target=sending, args=(soc_conn,))
-    rec_thread.start()
-    send_thread.start()
+            rec_thread = threading.Thread(target=receiving, args=(con_user, soc_conn))
+            send_thread = threading.Thread(target=sending, args=('me', soc_conn,))
+            rec_thread.start()
+            send_thread.start()
 
-    shutdown = True
-    rec_thread.join()
-    send_thread.join()
-    soc_conn.close()
+            shutdown = True
+            rec_thread.join()
+            send_thread.join()
+            soc_conn.close()
+        ans = input("Do you want to continue?(y/n): ")
 
 except redis.RedisError as e:
     traceback.print_exc()
